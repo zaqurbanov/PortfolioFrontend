cac1cf95973c98098bae681f61ac157efb0430dc
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-11-27 18:34:33 +0400

    zaur/fixBug/

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import Navbar from '#components/Navbar/Navbar';
 
 function App() {
   
-  const [count, setCount] = useState(8)
-  console.log(count);
   return (
     <>
 
