86aced9ee2f2b1a4188ace6f8fd3199a8ad9f18d
zaurqurbanov <qurbanovzaur078@gmail.com>
2025-12-11 20:58:46 +0400

    zaur/fix:buildBug

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,6 @@
 import NavIcon from './NavIcon'
 import Navlink from './Navlink'
 import useCurrentDate from '#hooks/useCurrentDate'
-import { Apple } from 'lucide-react'
 
 const Navbar = () => {
   const { currentDate } = useCurrentDate()
